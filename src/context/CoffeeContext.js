16549c992c4d8bb4cdb8bbbc8a4965f8e25dbc69
Christopher Hauser <chauser6@gmail.com>
2021-11-30 12:29:06 -0500

    phase 1 done

--- src/context/CoffeeContext.js
+++ src/context/CoffeeContext.js
@@ -1,7 +1,10 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useContext } from 'react';
 import coffeeBeans from '../mockData/coffeeBeans.json';
 
 export const CoffeeContext = createContext();
+
+export const useCoffeeContext = () => useContext(CoffeeContext);
+
 
 export default function CoffeeProvider(props) {
   const [coffeeBean, setCoffeeBean] = useState(coffeeBeans[0]);
