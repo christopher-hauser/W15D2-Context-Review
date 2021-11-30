3b5c30b4a0f8f6ca2319fc3a1835b2ef67a50626
David Nash <hisownspace@gmail.com>
2021-11-30 13:03:17 -0500

    finished project

--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
+import { useCoffeeContext } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { setCoffeeBeanId, coffeeBean } = useCoffeeContext();
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
-      >
+        value={coffeeBean.id}
+        onChange={(e) => {
+          setCoffeeBeanId(e.target.value);
+        }}
+        >
         {coffeeBeans.map(bean => (
           <option
-            key={bean.id}
-            value={bean.id}
+          key={bean.id}
+          value={bean.id}
           >
             {bean.name}
           </option>
